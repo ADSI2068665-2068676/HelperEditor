7f4f68da2bf677b028eda81fbf1eee2f9c6412b3
carlosfluna <carlosfluna@misena.edu.co>
2021-03-16 07:51:53 -0500

    se creo el HomeController

---
+++ HelperEditor/HelperEditor/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using HelperEditor.Models;
+
+namespace HelperEditor.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(FormCollection coleccion)
+        {
+            MantenimientoPersona ma = new MantenimientoPersona();
+            Persona per = ma.Retornar(int.Parse(coleccion["Codigo"].ToString()));
+
+            if (per != null)
+                return View("EditarPersona", per);
+            else
+                return RedirectToAction("Index");
+        }
+    }
+}
